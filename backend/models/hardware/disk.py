721e9994ef046d336139d75fe9683ee9de4636cd
xsalles <xs.salles@gmail.com>
2025-05-23 15:52:26 -0300

    feat: created the table of disk with id, name, size, type, usage_percent

---
+++ backend/models/hardware/disk.py
@@ -0,0 +1,15 @@
+from database import db
+
+class Disk(db.Model):
+    __tablename__ = 'disks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    size = db.Column(db.String(100), nullable=True)
+    type = db.Column(db.String(50), nullable=True)
+    usage_percent = db.Column(db.String(10), nullable=True)
+
+    def to_dict(self):
+        return {
+            col.name: getattr(self, col.name) for col in self.__table__.columns
+        }
