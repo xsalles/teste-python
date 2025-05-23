a44687f7390f53b5f49394f185eec4fad66df2fe
xsalles <xs.salles@gmail.com>
2025-05-23 15:49:33 -0300

    feat

--- backend/models/hardware/ram_memory.py
+++ backend/models/hardware/ram_memory.py
@@ -1,12 +1,12 @@
 from database import db
 
-class CPU(db.Model):
-    __tablename__ = 'cpus'
+class ram_emory(db.Model):
+    __tablename__ = 'ram_memories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=True)
-    cores = db.Column(db.Integer, nullable=True)
-    usage_percent = db.Column(db.String(10), nullable=True)
+    size = db.Column(db.String(10), nullable=True)
+    usage_percent = db.Column(db.Float, nullable=True)
 
     def to_dict(self):
         return {
