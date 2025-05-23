3f6678da91bd3bbaffec8ef0c9a388c004cc2edc
xsalles <xs.salles@gmail.com>
2025-05-22 22:42:52 -0300

    feat: added enviromental variables

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import SystemInfo
 from database import db, init_db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/absolute'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 db.init_app(app)
