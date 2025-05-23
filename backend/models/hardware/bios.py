4c627a32ff8913416f5eb9398040b2340d2e7899
xsalles <xs.salles@gmail.com>
2025-05-23 15:56:57 -0300

    feat: created the table of bios with id, version, release_date.

---
+++ backend/models/hardware/bios.py
@@ -0,0 +1,13 @@
+from database import db
+
+class BIOS(db.Model):
+    __tablename__ = 'bios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    version = db.Column(db.String(100), nullable=True)
+    release_date = db.Column(db.String(100), nullable=True)    
+    
+    def to_dict(self):
+        return {
+            col.name: getattr(self, col.name) for col in self.__table__.columns
+        }
