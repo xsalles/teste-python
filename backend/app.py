29e5e7377801e2a9addf7b6039c5362b7edd4d94
xsalles <xs.salles@gmail.com>
2025-05-22 22:38:56 -0300

    feat: created two routes, one that serves to post data and another to get data

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from model import SystemInfo
+from database import db, init_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/absolute'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
+db.init_app(app)
+init_db(app)
+
+@app.route('/system_info', methods=['POST'])
+def create_system_info():
+    data = request.get_json()
+    
+    system = SystemInfo(**data)
+    
+    db.session.add(system)
+    
+    db.session.commit()
+    
+    return jsonify({"message": "System information received successfully!"}), 201
+
+def get_system_info():
+    systems = SystemInfo.query.all()
+    
+    return jsonify([s.to_dict() for s in systems]), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
