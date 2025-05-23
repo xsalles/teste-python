bfdb00d9057a0541c1f409ff3a79f51397935883
xsalles <xs.salles@gmail.com>
2025-05-23 15:54:37 -0300

    feat: created the table of network with id, name mac_address, ip_address

---
+++ backend/models/hardware/network.py
@@ -0,0 +1,14 @@
+from database import db
+
+class Network(db.Model):
+    __tablename__ = 'networks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    ip_address = db.Column(db.String(100), nullable=True)
+    mac_address = db.Column(db.String(100), nullable=True)
+
+    def to_dict(self):
+        return {
+            col.name: getattr(self, col.name) for col in self.__table__.columns
+        }
