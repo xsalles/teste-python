a70122da932fa1ff341a0e9399b848826da48773
xsalles <xs.salles@gmail.com>
2025-05-24 21:53:58 -0300

    feat: created a function that manipulate information depending on the context(operational system).

---
+++ client/helpers/installed_softwares.py
@@ -0,0 +1,19 @@
+import platform
+from util import get_installed_software_windows, get_installed_software_linux, get_installed_software_mac
+
+def get_installed_softwares():
+    os = platform.system().lower()
+    
+    try:
+        if os == "windows":
+            return get_installed_software_windows()
+        elif os == "linux":
+            return get_installed_software_linux()
+        elif os == "darwin": 
+            return get_installed_software_mac()
+        else:
+            print(f"Unsupported OS: {os}")
+            return []
+    except Exception as e:
+        print(f"Error retrieving installed software: {e}")
+        return []
