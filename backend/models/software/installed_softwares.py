c88378c89484fbe323557a9e84f3e9b46966f42f
xsalles <xs.salles@gmail.com>
2025-05-24 21:43:23 -0300

    refactored the type of installation date because it could be'None'.

--- backend/models/software/installed_softwares.py
+++ backend/models/software/installed_softwares.py
@@ -4,9 +4,9 @@ class InstalledSoftware(db.Model):
     __tablename__ = 'installed_softwares'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    version = db.Column(db.String(50), nullable=False)
-    installation_date = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    version = db.Column(db.String(50), nullable=True)
+    installation_date = db.Column(db.String(100), nullable=True)
     device_id = db.Column(db.Integer, db.ForeignKey('devices.id'), nullable=False)
 
     def to_dict(self):
