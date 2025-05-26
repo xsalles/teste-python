68bb789478e5ded727c7115b42cb409434a82edb
xsalles <xs.salles@gmail.com>
2025-05-26 16:08:57 -0300

    feat: added logged_users into database

--- backend/db_config.py
+++ backend/db_config.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 def init_db(app):
     with app.app_context():
         from models.hardware import cpu, bios, disk, network, ram_memory
-        from models.software import installed_softwares, updates
+        from models.software import installed_softwares, updates, logged_users
         from models.location import Location
 
         db.create_all()
