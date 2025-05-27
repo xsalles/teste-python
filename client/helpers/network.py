4b07f5fe6fa528e7ed456f29a52577849adf3ff4
xsalles <xs.salles@gmail.com>
2025-05-26 22:39:34 -0300

    feat: refactored network function

--- client/helpers/network.py
+++ client/helpers/network.py
@@ -1,17 +1,29 @@
 import psutil
+import socket
 
 def get_network_info():
     try:
         net_if_addrs = psutil.net_if_addrs()
         for interface, addresses in net_if_addrs.items():
+            ip = None
+            mac = None
             for addr in addresses:
-                if addr.family == psutil.AF_INET:  # IPv4
-                    return {
-                        "name": interface,
-                        "ip_address": addr.address,
-                        "mac_address": addr.address if addr.family == psutil.AF_LINK else "Unknown"
-                    }
+                if addr.family == socket.AF_INET:
+                    ip = addr.address
+
+                elif hasattr(psutil, "AF_LINK") and addr.family == psutil.AF_LINK:
+                    mac = addr.address
+                elif addr.family == -1:  
+                    mac = addr.address
+            if ip and mac:
+                return {
+                    "name": interface,
+                    "ip_address": ip,
+                    "mac_address": mac
+                }
         return {"name": "Unknown", "ip_address": "0.0.0.0", "mac_address": "Unknown"}
     except Exception as e:
         print(f"Error getting Network info: {e}")
         return {"name": "Unknown", "ip_address": "0.0.0.0", "mac_address": "Unknown"}
+
+print(get_network_info())
